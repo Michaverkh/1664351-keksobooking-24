dc572fb1576b476cdc0c03ed4b5e06c986b66096
Michaverkh <michawerkh@gmail.com>
2021-10-15 08:15:43 +0300

    Работа по замечанием наставника. Добавлен фуекционал активного/неактивного состояния страницы

--- js/card-creation.js
+++ js/card-creation.js
@@ -11,51 +11,49 @@ const translate = {
   hotel: 'Отель',
 };
 
+// Функция для фильтрации элементов шаблона, согласно данным из объекта
 
-similarAds.forEach((Ad) => {
+const getFiltredElements = (template, features) => {
+  template.forEach((item) => {
+    const isExist = features.some((feature) =>
+      item.classList.contains(`popup__feature--${feature}`),
+    );
+    if(!isExist) {
+      item.remove();
+    }
+  });
+};
+
+similarAds.forEach((ad) => {
   const cardElement = cardTemplate.cloneNode(true);
 
-  cardElement.querySelector('.popup__avatar').src = Ad.autor.avatar;
-  cardElement.querySelector('.popup__title').textContent = Ad.offer.title;
-  cardElement.querySelector('.popup__text--address').textContent = Ad.offer.address;
-  cardElement.querySelector('.popup__text--price').textContent = `${Ad.offer.price}₽/ночь`;
-  cardElement.querySelector('.popup__type').textContent = translate[Ad.offer.type];
-  cardElement.querySelector('.popup__text--capacity').textContent = `${Ad.offer.rooms} комнаты для ${Ad.offer.guests} гостей`;
-  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${Ad.offer.checkin}, выезд до ${Ad.offer.checkout}`;
+  cardElement.querySelector('.popup__avatar').src = ad.author.avatar;
+  cardElement.querySelector('.popup__title').textContent = ad.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = ad.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = `${ad.offer.price}₽/ночь`;
+  cardElement.querySelector('.popup__type').textContent = translate[ad.offer.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
+  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`;
 
   const popupFeatures = cardElement.querySelector('.popup__features');
   const popupFeatureItems = popupFeatures.querySelectorAll('.popup__feature');
-  const featureClass = 'popup__feature--';
 
-  // Функция для фильтрации элементов шаблона, согласно данным из объекта
+  getFiltredElements(popupFeatureItems, ad.offer.features);
 
-  const getFiltredElements = (templateColection, objectItems, className) => {
-    templateColection.forEach((templateItem) => {
-      const isExisting = objectItems.some((objectItem) =>
-        templateItem.classList.contains(`${className}${objectItem}`),
-      );
-      if(!isExisting) {
-        templateItem.remove();
-      }
-    });
-  };
-
-  getFiltredElements(popupFeatureItems, Ad.offer.features, featureClass);
-
-  cardElement.querySelector('.popup__description').textContent = Ad.offer.description;
+  cardElement.querySelector('.popup__description').textContent = ad.offer.description;
   const popupPhotos = cardElement.querySelector('.popup__photos'); //Блок с фотографиями
   const popupPhotoItem = cardElement.querySelector('.popup__photo'); //сама фотография
-  const collectionPhotos = Ad.offer.photos;
+  const {photos} = ad.offer;
 
-  if (collectionPhotos.length === 0) {
+  if (photos.length === 0) {
     popupPhotoItem.remove();
   } else {
-    collectionPhotos.forEach((collectionPhoto) => {
+    photos.forEach((photo) => {
       const newPhoto = popupPhotoItem.cloneNode(true);
-      newPhoto.src = collectionPhoto;
+      newPhoto.src = photo;
       popupPhotos.appendChild(newPhoto);
-      popupPhotoItem.remove();
     });
+    popupPhotoItem.remove();
   }
 
   similarAdsFragment.appendChild(cardElement);
