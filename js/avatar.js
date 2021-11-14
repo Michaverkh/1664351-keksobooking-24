45f8088ea64d4e91c06735acc218071308ee066d
Michaverkh <michawerkh@gmail.com>
2021-11-14 20:27:24 +0300

    финальные правки

--- js/avatar.js
+++ js/avatar.js
@@ -1,12 +1,12 @@
 const fileChooser = document.querySelector('input[type=file]');
 const preview = document.querySelector('.setup-user-pic');
-const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+const FILE_TYPE = ['gif', 'jpg', 'jpeg', 'png'];
 
 fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
 
-  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+  const matches = FILE_TYPE.some((it) => fileName.endsWith(it));
 
   if (matches) {
     preview.src = URL.createObjectURL(file);
